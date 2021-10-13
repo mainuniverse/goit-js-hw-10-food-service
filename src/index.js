a68c5b55e571763de696ad6a00cd7577f7936e5d
Maria <54138760+mainuniverse@users.noreply.github.com>
2021-10-13 17:59:34 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -9,19 +9,16 @@ const markup = menuCardFile(mainMenu);
 const menuEl = document.querySelector('.js-menu');
 menuEl.insertAdjacentHTML('beforeend', markup);
 
-// DATA
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
   };
 
-// Переключатель тем (в DOM)
-const inputRef = document.querySelector('#theme-switch-toggle');
 
-// Слушатель события на инпуте
+const inputRef = document.querySelector('#theme-switch-toggle');
 inputRef.addEventListener('change', changeInput);
 
-// Проверка переключателя
+// check event toggle
 function changeInput(event) {
   event.currentTarget.checked ? checkedInput() : notCheckedInput();
 }
@@ -58,7 +55,7 @@ function currentTheme() {
 currentTheme();
 
 
- //localStorage//
+ //localStorage
  const savedData = localStorage.getItem('my-data');
  const parsedData = JSON.parse(savedData);
  document.body.classList.add(parsedData);
@@ -67,26 +64,3 @@ currentTheme();
  }
 
 
-//  const themeToggle = document.getElementById('theme-switch-toggle');
-//  //Listeners//
-//  const palleteComtainer = document.querySelector('.js-menu');
-//  const onToggleChange = themeToggle.addEventListener('change', themeChange);
-//  const menuMarkup = createMenuCards(mainMenu);
-//  palleteComtainer.insertAdjacentHTML('beforeend' , menuMarkup);
-
-// //function//
-
-// function themeChange(){
-//     if(document.body.classList.contains(Theme.DARK)|| document.body.classList === ''){
-//         document.body.classList.toggle(Theme.LIGHT);
-//         document.body.classList.remove(Theme.DARK);
-//         localStorage.setItem('my-data', JSON.stringify(Theme.LIGHT));
-//         return
-//     }
-//   document.body.classList.toggle(Theme.DARK);
-//   document.body.classList.remove(Theme.LIGHT);
-//   localStorage.setItem('my-data', JSON.stringify(Theme.DARK));
-// };
-// function createMenuCards(mainMenu){
-//     return mainMenu.map(menuCardFile).join('')
-// }
