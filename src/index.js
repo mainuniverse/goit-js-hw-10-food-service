5128670a4fddf7abcc1c07abf59f44e13157b995
Maria <54138760+mainuniverse@users.noreply.github.com>
2021-10-16 23:20:20 +0300

    update current theme

--- src/index.js
+++ src/index.js
@@ -5,62 +5,71 @@ import menuCardFile from './menu-card.hbs';
 import mainMenu from './menu.json';
 
 const markup = menuCardFile(mainMenu);
-
 const menuEl = document.querySelector('.js-menu');
 menuEl.insertAdjacentHTML('beforeend', markup);
-
+const mainTheme = document.querySelector('body');
+const inputRef = document.querySelector('#theme-switch-toggle');
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
   };
-
-
-const inputRef = document.querySelector('#theme-switch-toggle');
-inputRef.addEventListener('change', changeInput);
-
-// check event toggle
-function changeInput(event) {
-  event.currentTarget.checked ? checkedInput() : notCheckedInput();
-}
-
-// add dark theme
-function checkedInput() {
-  document.body.classList.add(Theme.DARK);
-  document.body.classList.remove(Theme.LIGHT);
-  localStorage.setItem('theme', Theme.DARK);
-  inputRef.checked = true;
-}
-
-// add light theme
-function notCheckedInput() {
-  document.body.classList.add(Theme.LIGHT);
-  document.body.classList.remove(Theme.DARK);
-  localStorage.setItem('theme', Theme.LIGHT);
-  inputRef.checked = false;
-}
-
-// theme by defoult
+ 
 function currentTheme() {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === Theme.LIGHT || savedTheme === null) {
-    notCheckedInput();
-    return;
-  }
-
-  if (savedTheme === Theme.DARK) {
-    checkedInput();
-    return;
-  }
+    const savedTheme = localStorage.getItem('theme');
+    if (!savedTheme) {
+        mainTheme.classList.add(Theme.LIGHT)
+    } else {
+        mainTheme.classList.add(savedTheme)
+    }
+    if (mainTheme.classList.contains(Theme.DARK)) {
+        inputRef.checked = true;
+    }
 }
 currentTheme();
 
+inputRef.addEventListener('change', () => {
+    mainTheme.classList.toggle(Theme.DARK);
+    mainTheme.classList.toggle(Theme.LIGHT);
 
- //localStorage
- const savedData = localStorage.getItem('my-data');
- const parsedData = JSON.parse(savedData);
- document.body.classList.add(parsedData);
- if(document.body.classList.contains(Theme.DARK)){
-    themeToggle.checked = true;
- }
+    if (mainTheme.classList.contains(Theme.DARK)) {
+        localStorage.setItem('theme', Theme.DARK)
+    } else {
+       localStorage.setItem('theme', Theme.LIGHT) 
+    }
+})
+// //function//
+
+// function changeInput(){
+//     if(document.body.classList.contains(Theme.DARK)|| document.body.classList === ''){
+//         document.body.classList.toggle(Theme.LIGHT);
+//         document.body.classList.remove(Theme.DARK);
+//         localStorage.setItem('my-data', JSON.stringify(Theme.LIGHT));
+//         return
+//     }
+//   document.body.classList.toggle(Theme.DARK);
+//   document.body.classList.remove(Theme.LIGHT);
+//   localStorage.setItem('my-data', JSON.stringify(Theme.DARK));
+// };
+// // theme by defoult
+// function currentTheme() {
+//   const savedTheme = localStorage.getItem('theme');
+//   if (savedTheme === Theme.LIGHT || savedTheme === null) {
+//     notCheckedInput();
+//     return;
+//   }
+
+//   if (savedTheme === Theme.DARK) {
+//     checkedInput();
+//     return;
+//   }
+// }
+// currentTheme();
+//  //localStorage
+//  const savedData = localStorage.getItem('my-data');
+//  const parsedData = JSON.parse(savedData);
+//  document.body.classList.add(parsedData);
+//  if(document.body.classList.contains(Theme.DARK)){
+//     themeToggle.checked = true;
+//  }
 
 
